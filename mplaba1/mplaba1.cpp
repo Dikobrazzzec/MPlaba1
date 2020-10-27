863d591e1b768d1ebb3aec68b38f9798821562e4
Dikobrazzzec <zasxcd1307@mail.ru>
2020-10-27 18:00:55 +0300

    final

--- mplaba1/mplaba1.cpp
+++ mplaba1/mplaba1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Point {
 public:
-	Point();
+	Point() {};
 	int x;
 	int y;
 	Point(int x_, int y_) :x(x_), y(y_) {};
@@ -18,34 +18,64 @@ private:
 	Point RU;
 	Point RL;
 public:
-	bool a;
-	Rectangle(bool a_) : a(a_) {};
-	Rectangle* Initializer(Rectangle* initRecptr) {
-		cout << "Enter X and Y left up, left low, right up, right low points" << endl;
+	Rectangle() {};
+	Rectangle* Creator(Rectangle* creatPtr) {
+		cout << "Enter X and Yleft low, right up points" << endl;
 		cout << "The sides must be parallel to the axes" << endl;
-		cout << "left up" << endl;
-		cin >> initRecptr->LU.x;
-		cin >> initRecptr->LU.y;
 		cout << "left low" << endl;
-		cin >> initRecptr->LL.x;
-		cin >> initRecptr->LL.y;
+		cin >> creatPtr->LL.x;
+		cin >> creatPtr->LL.y;
 		cout << "right up" << endl;
-		cin >> initRecptr->RU.x;
-		cin >> initRecptr->RU.y;
-		cout << "right low" << endl;
-		cin >> initRecptr->RL.x;
-		cin >> initRecptr->RL.y;
-		if (LL.y != RL.y || LU.y != RU.y || LL.x != LU.x || RL.x != RU.x) {
-			cout << "Enter correct coordinates" << endl;
+		cin >> creatPtr->RU.x;
+		cin >> creatPtr->RU.y;
+		creatPtr = Maker(creatPtr);
+		return creatPtr;
+	}
+
+	Rectangle* Maker(Rectangle* makeptr) {
+		makeptr->LU.x = makeptr->LL.x;
+		makeptr->RL.x = makeptr->RU.x;
+		makeptr->LU.y = makeptr->RU.y;
+		makeptr->RL.y = makeptr->LL.y;
+		return makeptr;
+	}
+
+	void Tester (Rectangle * rec1, Rectangle * rec2) {
+		if (rec1->LL.x >= rec2->LL.x) {
+			Intersection(rec2, rec1);
+		}
+		if (rec1->LL.x <= rec2->LL.x) {
+			Intersection(rec1, rec2);
+		}
+	}
+
+	Rectangle* Intersection (Rectangle* L, Rectangle* R) {
+		Rectangle Inter;
+		Rectangle* InterPtr = &Inter;
+		if (L->RL.x <= R->LL.x || L->RL.y >= R->LU.y || L->RU.y <= R->LL.y) {
+			cout << "Rectangle havn't intersection rectangle" << endl;
 			return nullptr;
-		}
-		else {
-			return initRecptr;
-		}
+		} 
+		if (L->LL.y >= R->LL.y) { InterPtr->LL.x = R->LL.x; InterPtr->LL.y = L->LL.y; }  //left low
+		if (L->LL.y < R->LL.y) { InterPtr->LL.x = R->LL.x; InterPtr->LL.y = R->LL.y; }   //------
+		if (L->RU.y <= R->RU.y) { InterPtr->RU.y = L->RU.y; } //y right up
+		if (L->RU.y > R->RU.y) { InterPtr->RU.y = R->RU.y; }  //------
+		if (L->RU.x >= R->RU.x) { InterPtr->RU.x = R->RU.x; }//x right up
+		if (L->RU.x <= R->RU.x) { InterPtr->RU.x = L->RU.x; }//-------
+		InterPtr = Maker(InterPtr);
+		cout << &Inter << endl;
+		return InterPtr;
 	}
 };
 
-int main()
-{
-	Rectangle A(true);
+void starter() {
+	Rectangle A;
+	A.Creator(&A);
+	Rectangle B;
+	B.Creator(&B);
+	A.Tester(&A, &B);
+}
+
+int main(){
+	starter();
 }
